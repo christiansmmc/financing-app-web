2d2374cc38991b60b9edbf54c82f414f705b2285
Christian Sequeira <csequeira153@gmail.com>
2024-06-24 22:21:25 -0300

    feat: refactor on login/register screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,7 @@ import {useLoginMutation} from "@/api/user";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Button} from "@/components/ui/button";
+import GraphicImage from "@/components/images/graphicImage";
 import {useRouter} from "next/navigation";
 import {LoginData, LoginSchema} from "@/schemas/schemas";
 import Header from "@/components/header";
@@ -35,35 +36,46 @@ export default function Page() {
     return (
         <main className={"h-screen flex flex-col"}>
             <Header buttonText={"Crie sua conta"} buttonOnClick={handleGoRegister}/>
-            <div className={"flex flex-1 items-center justify-center"}>
-                <form
-                    onSubmit={handleSubmit(handleLogin)}
-                    className={"flex flex-col gap-5 w-4/5 border p-10 rounded shadow-custom-heavy " +
-                        "lg:w-2/5 " +
-                        "xl:w-1/3 " +
-                        "2xl:w-1/5"}>
-                    <div>
-                        <div className={"text-gray-600"}>Email<span className={"text-red-500"}>*</span></div>
-                        <input
-                            type={"email"}
-                            placeholder={"Digite aqui o email"}
-                            className={"bg-gray-200 w-full h-10 rounded pl-2 text-sm"}
-                            {...register("email")}/>
-                        {errors.email &&
-                            <span className={"text-xs text-red-500"}>{errors.email.message}</span>}
-                    </div>
-                    <div>
-                        <div className={"text-gray-600"}>Senha<span className={"text-red-500"}>*</span></div>
-                        <input
-                            type={"password"}
-                            placeholder={"Digite aqui a senha"}
-                            className={"bg-gray-200 w-full h-10 rounded pl-2 text-sm"}
-                            {...register("password")}/>
-                        {errors.password &&
-                            <span className={"text-xs text-red-500"}>{errors.password.message}</span>}
-                    </div>
-                    <Button type={"submit"} className={"text-md"}>Entrar</Button>
-                </form>
+            <div className="h-screen flex">
+                <div className="hidden
+                lg:flex lg:justify-end lg:items-center lg:w-1/2">
+                    <GraphicImage className="w-5/6"/>
+                </div>
+                <div className="flex w-full justify-center items-center
+                lg:w-1/2 lg:shadow-custom-heavy lg:ml-10
+                2xl:justify-start 2xl:pl-28">
+                    <form
+                        className="flex flex-col gap-5 w-5/6 border border-gray-100 p-5 shadow-custom-heavy
+                        sm:w-2/3 sm:p-10
+                        md:w-1/2
+                        lg:p-10 lg:w-4/5
+                        2xl:w-3/5
+                        3xl:w-2/5"
+                        onSubmit={handleSubmit(handleLogin)}
+                            >
+                        <div>
+                            <div className={"text-gray-600"}>Email<span className={"text-red-500"}>*</span></div>
+                            <input
+                                type={"email"}
+                                placeholder={"Digite aqui o email"}
+                                className={"bg-gray-200 w-full h-10 rounded pl-2 text-sm"}
+                                {...register("email")}/>
+                            {errors.email &&
+                                <span className={"text-xs text-red-500"}>{errors.email.message}</span>}
+                        </div>
+                        <div>
+                            <div className={"text-gray-600"}>Senha<span className={"text-red-500"}>*</span></div>
+                            <input
+                                type={"password"}
+                                placeholder={"Digite aqui a senha"}
+                                className={"bg-gray-200 w-full h-10 rounded pl-2 text-sm"}
+                                {...register("password")}/>
+                            {errors.password &&
+                                <span className={"text-xs text-red-500"}>{errors.password.message}</span>}
+                        </div>
+                        <Button type={"submit"} className={"text-md"}>Entrar</Button>
+                    </form>
+                </div>
             </div>
         </main>
     );
